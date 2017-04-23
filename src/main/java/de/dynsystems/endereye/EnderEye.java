5760e7e33beb7fabbfdaf8a144555bfd02ab29d6
DevAlexHD <devalexhd@gmail.com>
2017-04-23 20:45:45 +0200

    1

--- src/main/java/de/dynsystems/endereye/EnderEye.java
+++ src/main/java/de/dynsystems/endereye/EnderEye.java
@@ -1,7 +1,19 @@
 package de.dynsystems.endereye;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * Created by Alexander on 23.04.2017.
  */
-public class EnderEye {
+public class EnderEye extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
 }
